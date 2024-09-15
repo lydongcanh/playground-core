09d3ce8fbf31fdf502eceb753cee8d99f5f35b57
Ted Ly <lydongcanh@gmail.com>
2024-09-15 22:23:12 +0700

    Add auth0 terraform provider.

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,15 @@ terraform {
   required_providers {
     neon = {
       source = "kislerdm/neon"
+      version = "0.5.0"
+    }
+    auth0 = {
+      source = "auth0/auth0"
+      version = "1.6.1"
     }
   }
 }
 
 provider "neon" {}
+
+provider "auth0" {}
